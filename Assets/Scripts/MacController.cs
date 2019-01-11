a760d7dbc494d36ee94a4acc08a28c936b90ad5d
Q <q@mainegame.club>
2019-01-11 17:51:44 -0500

    so I put in a git ignore but it's still pushing meta data shit what the fuck

---
+++ Assets/Scripts/MacController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MacController : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+    [SerializeField] private GameObject gameCon;
+
+    private WorldMover gameScript;
+
+    private Rigidbody rb;
+    private bool isGrounded = false;
+    private Vector3 startingPos;
+
+    private void Awake()
+    {
+        isGrounded = false;
+        startingPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        if (gameCon != null)
+            gameScript = gameCon.GetComponent<WorldMover>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Used to reset the character position 
+    public void ResetCharacter()
+    {
+        gameObject.transform.position = startingPos;
+        gameScript.ResetWorld();
+    }
+
+    private void StartMoving()
+    {
+        isGrounded = true;
+        //gameCon.GetComponent<MacController>().isGrounded = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            StartMoving();
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Killbox")
+        {
+            ResetCharacter();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            ResetCharacter();
+        }
+
+        if (other.gameObject.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKey(KeyCode.D))
+                rb.AddForce(Vector3.right * speed, ForceMode.Impulse);
+            if (Input.GetKey(KeyCode.A))
+                rb.AddForce(Vector3.left * speed, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMover : MonoBehaviour
+{
+
+    [SerializeField] Transform[] groundObjects;
+    [SerializeField] private GameObject macGO;
+    [SerializeField] private float lerpSpeed = 5;
+
+    private bool isGrounded;
+    private Vector3 startingPos;
+
+    private void Awake()
+    {
+        startingPos = transform.position;
+        isGrounded = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.back * 20f * Time.deltaTime);
+        //https://www.unity3dtips.com/unity-fix-movement-stutter/
+        transform.position = Vector3.Lerp(transform.position, Vector3.back * 300, (Time.deltaTime * lerpSpeed)/100f);
+    }
+
+    public void ResetWorld()
+    {
+        //macGO.GetComponent<MacController>().ResetCharacter();
+        transform.position = startingPos;
+    }
+}
