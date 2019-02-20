45d43404bd9fa911c27b37428d97d07ccdea6a43
MEMIC1\QSC <q@mainegame.club>
2019-02-20 12:02:04 -0500

    Made the individual things move but it needs to be smoothed

--- Assets/Scripts/WorldMover.cs
+++ Assets/Scripts/WorldMover.cs
@@ -41,23 +41,19 @@ public class WorldMover : MonoBehaviour
         startingPos = transform.position;
         slideChildGOs = groundTrans.childCount;
         //lengthFlo = groundTrans.GetChild(0).gameObject.GetComponentInChildren<MeshCollider>().bounds.size.y;
-        
-        //Get number of children and length of them
-       
-
     }
 
     void Start()
     {
-        StartCoroutine(WaitAndMove(1f, slideChildGOs));
+        //StartCoroutine(WaitAndMove(1f, slideChildGOs));
+        StartCoroutine(ContinuouslyMove(slideChildGOs));
     }
 
     void Update()
     {
-
         //https://www.unity3dtips.com/unity-fix-movement-stutter/
-        //transform.position = Vector3.Lerp(transform.position, Vector3.back * 300, (Time.deltaTime * lerpSpeed)/100f);
-        transform.position += Vector3.back * (lerpSpeed / 10);
+        //not this one -- transform.position = Vector3.Lerp(transform.position, Vector3.back * 300, (Time.deltaTime * lerpSpeed)/100f);
+        //transform.position += Vector3.back * (lerpSpeed / 10);
     }
 
     public void ResetWorld()
@@ -65,52 +61,46 @@ public class WorldMover : MonoBehaviour
         //macGO.GetComponent<MacController>().ResetCharacter();
         transform.position = startingPos;
     }
-
-
     
+    private IEnumerator ContinuouslyMove(int slideCount)
+    {
+        while(repeat)
+        {
+            slideCount++;
+            if (slideCount > slideChildGOs - 1)
+                slideCount = 0;
+
+            groundTrans.GetChild(slideCount).transform.position += Vector3.back * 50f * Time.deltaTime;
+            yield return new WaitForSeconds(Time.smoothDeltaTime);
+        }
+    }
+
+
     // loop through background
     private IEnumerator WaitAndMove(float time, int slideCount)
     {
-        Debug.Log("SlideCount before: " + slideCount);
-
-        // Subtracting 1 because we start counting from 0 but we count the GO starting at 1
-      
+        //Debug.Log("SlideCount before: " + slideCount);
 
         while (repeat)
         {
             slideCount++;
+            // Subtracting 1 because we start counting from 0 but we count the GO starting at 1 
             if (slideCount > slideChildGOs - 1)
             {
                 slideCount = 0;
             }
-
-            groundTrans.GetChild(slideCount).transform.position += new Vector3(0, 0, 458f);
+        
+            groundTrans.GetChild(slideCount).transform.position += new Vector3(0, 0, 230f);
 
             Debug.Log("SlideCount after: " + slideCount);
-           
-
-            //Debug.Log("OnCoroutine: " + (int)Time.time);
             yield return new WaitForSeconds(2f);
         }
 
         groundTrans.GetChild(slideCount);
 
-        //slides[slideCount].transform.position += new Vector3(100f, 0f, 170f);
-
-
-     
-
-
-        //if (slideCount > slides.Capacity - 1)
-          //  slideCount = 0;
-
         Debug.Log("SlideParts 0: " + slides[0].gameObject.name);
-
         Debug.Log("SlidePart Capacity: " + (slides.Capacity));
-
-        //slideParts[slideParts.Capacity - 2].transform.SetSiblingIndex(0);
         Debug.Log("First Child: " + groundTrans.GetChild(0).name);
-
 
         yield return new WaitForSeconds(time);
     }
