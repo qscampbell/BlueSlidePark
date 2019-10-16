9fcaad59bbcbfe26121bc9c5a75b0d64b16bdc30
qatfish1 <qcampbell@utc4me.org>
2019-10-16 17:45:54 -0400

    added main menu with simple start button

---
+++ Assets/Scenes/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+
+    public void LoadSceneButton(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
